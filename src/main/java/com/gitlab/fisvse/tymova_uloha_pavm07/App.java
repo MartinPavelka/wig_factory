de92c27214b7f35e0442555c55c96ad842b9bf1e
Martin Pavelka <martypavelka@gmail.com>
2018-11-15 16:27:27 +0100

    Initial commit

---
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/App.java
@@ -0,0 +1,13 @@
+package com.gitlab.fisvse.tymova_uloha_pavm07;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
