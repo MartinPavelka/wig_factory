9bbd92017cc3f6e41312bc42c7acc4e1f932349c
Martin Pavelka <martypavelka@gmail.com>
2018-12-04 10:08:41 +0100

    Creating User Class 'interface'

---
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/users/Admin.java
@@ -0,0 +1,11 @@
+package com.gitlab.fisvse.tymova_uloha_pavm07.users;
+
+public class Admin {
+	String username;
+	String password;
+	
+	Admin() {
+		this.username = username;
+		this.password = password;
+	}
+}
--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/users/User.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/users/User.java
@@ -2,4 +2,42 @@ package com.gitlab.fisvse.tymova_uloha_pavm07.users;
 
 public class User {
 
+	private String name;
+	private String username;
+	private String password;
+	private boolean Gender; // 0 - female, 1 - male
+	private int userType;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isGender() {
+		return Gender;
+	}
+	public void setGender(boolean gender) {
+		Gender = gender;
+	}
+	public int getUserType() {
+		return userType;
+	}
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+	
+	
 }
