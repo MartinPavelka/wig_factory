78346748275e1ddb3a228231cbdd317f64da7fc6
Veronika Karásková <karv00@vse.cz>
2018-12-09 00:13:54 +0100

    filled Donation class /VeKa

--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/Donation.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/Donation.java
@@ -2,4 +2,23 @@ package com.gitlab.fisvse.tymova_uloha_pavm07.objects;
 
 public class Donation {
 
+	private String type;
+	private int amount;
+
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public void donate(int amount, String type) {
+		this.amount = amount;
+		this.type = type;
+	}
 }
