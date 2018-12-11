887e4a5f93f42638ac4c90367e6338e2fe24b73a
Pavel Dvoriak <dvop06@vse.cz>
2018-12-11 10:51:39 +0100

    adding comment to class Donation¨

--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/Donation.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/Donation.java
@@ -5,7 +5,7 @@ import com.gitlab.fisvse.tymova_uloha_pavm07.users.Donor;
 public class Donation {
 
 	private String type;
-	private int amount;
+	private int amount; //if money the unit is CZK, if hair its grams
 	private Donor owner;
 	
 	public Donation (String type, int amount, Donor owner) {
