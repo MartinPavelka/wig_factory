72f28c43328fc506202c1e82093dc705d24d4451
Ondřej Bergman <bero04@vse.cz>
2018-12-25 17:03:59 +0000

    Update Hair.java

--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/Hair.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/Hair.java
@@ -1,5 +1,51 @@
 package com.gitlab.fisvse.tymova_uloha_pavm07.objects;
 
-public class Hair {
+import com.gitlab.fisvse.tymova_uloha_pavm07.users.Donor;
 
+public class Hair {
+	
+	private Donor donatedBy;
+	private String color;
+	private boolean wasUsed;
+	private int length;
+	
+	public Hair (Donor donatedBy, String color, boolean wasUsed,int length) {
+		this.donatedBy = donatedBy;
+		this.color = color;
+		this.wasUsed = wasUsed;
+		this.length = length;
+	}
+
+	public Donor getDonatedBy() {
+		return donatedBy;
+	}
+
+	public void setDonatedBy(Donor donatedBy) {
+		this.donatedBy = donatedBy;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isWasUsed() {
+		return wasUsed;
+	}
+
+	public void setWasUsed(boolean wasUsed) {
+		this.wasUsed = wasUsed;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
 }
