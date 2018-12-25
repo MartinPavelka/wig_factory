da2a739281ccf0d194dacca3c403fccc0d8321c5
Ondřej Bergman <bero04@vse.cz>
2018-12-25 17:04:42 +0000

    Update EventWig.java

--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/EventWig.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/objects/EventWig.java
@@ -1,5 +1,40 @@
 package com.gitlab.fisvse.tymova_uloha_pavm07.objects;
 
+import com.gitlab.fisvse.tymova_uloha_pavm07.objects.Hair;
+
 public class EventWig {
 
+	private Hair hairReceived;
+	private int cost;
+	private int moneyReceived;
+	
+	public EventWig (Hair hairReceived, int cost, int moneyReceived) {
+		this.setHairReceived(hairReceived);
+		this.setCost(cost);
+		this.setMoneyReceived(moneyReceived);
+	}
+
+	public Hair getHairReceived() {
+		return hairReceived;
+	}
+
+	public void setHairReceived(Hair hairReceived) {
+		this.hairReceived = hairReceived;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public int getMoneyReceived() {
+		return moneyReceived;
+	}
+
+	public void setMoneyReceived(int moneyReceived) {
+		this.moneyReceived = moneyReceived;
+	}
 }
