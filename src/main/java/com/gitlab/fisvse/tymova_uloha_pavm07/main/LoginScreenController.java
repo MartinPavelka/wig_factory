933de6449b152c7f6a9bff992dab696a580679e0
Martin Pavelka <martypavelka@gmail.com>
2019-01-03 17:42:06 +0100

    Making registration work.

--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/main/LoginScreenController.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/main/LoginScreenController.java
@@ -1,32 +1,75 @@
 package com.gitlab.fisvse.tymova_uloha_pavm07.main;
 
+import java.util.HashMap;
+
 import com.gitlab.fisvse.tymova_uloha_pavm07.enums.Roles;
 import com.gitlab.fisvse.tymova_uloha_pavm07.main.Model.UserModel;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import javafx.util.Pair;
+import javafx.scene.control.ComboBox;
 
 public class LoginScreenController extends Controller {
 
 	@FXML TextField inputRegUsername,
 					inputRegPassword,
-					inputRegMail;
+					inputRegMail,
+					inputUsername,
+					inputPassword;
+	@FXML ComboBox<String> comboboxRole;
+	
+	private HashMap<String, String> roleMap = new HashMap<>();
+
+
+	// INIT
 
 	public void init() {
-		System.out.println("YUUUPPPIIIIIIII!!!! Login");
+		initRoleMap();
+		initComboBox();
+		clear();
+		comboboxRole.getSelectionModel().selectLast();
 	}
+	private void initRoleMap() {
+		roleMap.put("Pacient", "PATIENT");
+		roleMap.put("Donor", "DONOR");
+	}
+	@SuppressWarnings("unchecked")
+	private void initComboBox() {
+		for (String key : roleMap.keySet()) {
+			comboboxRole.getItems().add(key);
+		}
+	}
+
+	//
 	
+	public void clear() {
+		inputRegUsername.setText(null);
+		inputRegPassword.setText(null);
+		inputRegMail.setText(null);
+		inputUsername.setText(null);
+		inputPassword.setText(null);
+	}
+
 	public void onClickLogin() {
 //		this.router.setRoute("donor");
 	}
 	
 	public void onClickRegister() {
 		UserModel model = new UserModel();
+		String role = roleMap.get(comboboxRole.getSelectionModel().getSelectedItem());
+		System.out.println(role);
+		System.out.println(inputRegUsername.getText());
+		System.out.println(inputRegPassword.getText());
+		System.out.println(inputRegMail.getText());
+		
 		model.createUser(
 			inputRegUsername.getText(),
 			inputRegPassword.getText(),
 			inputRegMail.getText(),
-			Roles.getInstance().get("DONOR")
+			Roles.getInstance().get(role)
 		);
+		clear();
 	}
 }
