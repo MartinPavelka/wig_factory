275501a6d401ea283db0553da7806982a39029fa
Eva-Marie <evamarie.lacinova@gmail.com>
2019-01-05 17:47:31 +0100

    admin controller altered

--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/main/AdminScreenController.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/main/AdminScreenController.java
@@ -1,8 +1,30 @@
 package com.gitlab.fisvse.tymova_uloha_pavm07.main;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 import java.util.HashMap;
 
 public class AdminScreenController {
+
+	@FXML
+	MenuItem logoutFromMenu;
+	MenuItem endApplication;
+	TableView tableUsers;
+	TextField inputUserPasswordOld;
+	TextField inputUserPasswordNew;
+	TextField inputUserPaswordNewCheck;
+	TextField inputUserEmailNew;
+	TextField inputNewUserName;
+	TextField inputNewUserMail;
+	ComboBox newUserRole;
+	TextField inputNewUserPassword;
+
+
+
 	public void init() {
 		
 	}
