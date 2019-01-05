56fd7b400fd34837f9d662ef435a5c2f9b8d39d4
Martin Pavelka <martypavelka@gmail.com>
2019-01-05 19:51:03 +0100

    updating PatienScreenController

--- src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/main/PatientScreenController.java
+++ src/main/java/com/gitlab/fisvse/tymova_uloha_pavm07/main/PatientScreenController.java
@@ -7,38 +7,81 @@ import javafx.scene.control.TextField;
 
 import java.util.HashMap;
 
+import com.gitlab.fisvse.tymova_uloha_pavm07.main.Model.UserModel;
+
 public class PatientScreenController extends Controller{
 
-	@FXML
-	MenuItem logoutFromMenu;
-	MenuItem endApplication;
-	TextField inputUserPasswordOld;
-	TextField inputUserPasswordNew;
-	TextField inputUserPaswordNewCheck;
-	TextField inputUserEmailNew;
+	
+	@FXML MenuItem 
+		logoutFromMenu,
+		endApplication;
+	@FXML TextField 
+		inputUserPasswordOld,
+		inputUserPasswordNew,
+		inputUserPasswordNewCheck,
+		inputUserEmailNew;
 
 	public void init() {
-		
-	}
-	
-	public void donate() {
-		
+		clearAll();
 	}
 	
 	public void onClickChangeUserPassword() {
+		String oldPassword = inputUserPasswordOld.getText();
+		String newPassword = inputUserPasswordNew.getText();
+		String newPasswordCheck = inputUserPasswordNewCheck.getText();
 		
+		UserModel model = new UserModel();
+		if(model.login(app.currentUser.getUsername(), oldPassword) == null) {
+			alertErrorAndWait("Špatné heslo.");
+			return;
+		}
+		
+		if (!newPassword.equals(newPasswordCheck)) {
+			alertErrorAndWait("Hesla se neshodují.");
+			return;
+		}
+		
+		if (!app.currentUser.setPassword(newPassword)) {
+			alertErrorAndWait("Heslo nelze zmenit", "Objevila se neocekavana chyba, proto nic nebude...");
+			return;
+		};
+		
+		clearPasswordChange();
+		alertInfoAndWait("Heslo zmeneno.");
 	}
 	
 	public void onClickChangeUserMail() {
+		String mail = inputUserEmailNew.getText();
+
+		if(!new UserModel().setMail(app.currentUser.getId(), mail)){
+			alertErrorAndWait("Email nelze zmenit", "Objevila se neocekavana chyba, proto nic nebude...");
+			return;
+		}
 		
-	}
-
-	public void onClickLogout() {
-
+		app.currentUser.setMail(mail);
+		
+		clearEmailChange();
+		alertInfoAndWait("Email zmenen.");
 	}
 
 	public void onClickCreateNewOrder() {
 		// založit žádost o paruku a nahrát do DB
 	}
-
+	
+	public void onClickLogout() {
+		logout();
+	}
+	
+	private void clearAll() {
+		clearEmailChange();
+		clearPasswordChange();
+	}
+	private void clearEmailChange() {
+		inputUserEmailNew.clear();
+	}
+	private void clearPasswordChange() {
+		inputUserPasswordOld.clear();
+		inputUserPasswordNew.clear();
+		inputUserPasswordNewCheck.clear();
+	}
 }
