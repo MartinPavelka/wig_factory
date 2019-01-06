39e067adcd580dff32ea4a0c8dd95e9c40f15f76
Eva-Marie <evamarie.lacinova@gmail.com>
2019-01-06 16:54:37 +0100

    EmplyeeTest altered

--- src/test/java/com/gitlab/fisvse/tymova_uloha_pavm07/EmployeeTest.java
+++ src/test/java/com/gitlab/fisvse/tymova_uloha_pavm07/EmployeeTest.java
@@ -1,10 +1,19 @@
 package com.gitlab.fisvse.tymova_uloha_pavm07;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import com.gitlab.fisvse.tymova_uloha_pavm07.main.Model.OrdersModel;
+import com.gitlab.fisvse.tymova_uloha_pavm07.main.Model.ProjectsModel;
+import com.gitlab.fisvse.tymova_uloha_pavm07.main.Model.UserModel;
+import com.gitlab.fisvse.tymova_uloha_pavm07.objects.Project;
+import com.gitlab.fisvse.tymova_uloha_pavm07.users.User;
+
+import javafx.collections.ObservableList;
 
 
 /**
@@ -12,8 +21,13 @@ import org.junit.jupiter.api.Test;
  *
  */
 public class EmployeeTest {
-
-
+	
+	UserModel users = new UserModel();
+	OrdersModel orders = new OrdersModel();
+	ProjectsModel projects = new ProjectsModel();
+	ObservableList<Project> projectsList;
+	ObservableList<User> usersList;
+	
     @Before
     public void setUp()
     {
@@ -24,7 +38,24 @@ public class EmployeeTest {
     {
     }
     
-    public void testMultipleRequests() {
-    	
+    public void testMultipleOrders() {
+    	assertEquals(users.createUser("zamestnanec", "12345", "example@mail.org", 1), true);
+    	assertEquals(users.createUser("pacient1", "12345", "example@mail.org", 3), true);
+    	assertEquals(users.createUser("pacient2", "12345", "example@mail.org", 3), true);
+    	usersList = users.getAll();
+    	int id = 0;
+    	for (int i = 0; i < usersList.size(); i++) {
+    		if (usersList.get(i).getUsername().equals("zamestnanec")) {
+    			id = usersList.get(i).getId();
+    		}
+    	}
+    	projectsList = projects.getAll(id);
+    	int oldCount = projectsList.size();
+    	assertEquals(projects.addProject("projekt1", 100), true);
+    	assertEquals(projects.addProject("projekt1", 200), true);
+    	projectsList = projects.getAll(id);
+    	int newCount = projectsList.size();
+    	assertEquals(oldCount, newCount - 2);
+   	
     }
 }
