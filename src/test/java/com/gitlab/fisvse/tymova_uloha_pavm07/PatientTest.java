fc2b4d1f865a134bcaacd9faec2a7d18f97163b3
Eva-Marie <evamarie.lacinova@gmail.com>
2019-01-06 14:31:22 +0100

    test structure added

---
+++ src/test/java/com/gitlab/fisvse/tymova_uloha_pavm07/PatientTest.java
@@ -0,0 +1,31 @@
+package com.gitlab.fisvse.tymova_uloha_pavm07;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+
+/**
+ * Testovací třída pro třídu Patient.
+ *
+ */
+public class UserTest {
+
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+    
+    public void testRequestCreated() {
+    	
+    }
+}
+
+    
---
+++ src/test/java/com/gitlab/fisvse/tymova_uloha_pavm07/UserTest.java
@@ -0,0 +1,39 @@
+package com.gitlab.fisvse.tymova_uloha_pavm07;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+
+/**
+ * Testovací třída pro třídu User.
+ *
+ */
+public class UserTest {
+
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+    
+    
+    public void testSuccesfulRegustration() {
+    	
+    }
+    
+    public void testPasswordChange() {
+    	
+    }
+    
+    public void testEmailChange() {
+    	
+    }
+}
+
